418ec7007c4327fe7594bd3327ad57d6e24d77bf
Illia Katerynych <iliakat.kr@gmail.com>
2024-06-20 19:26:12 +0100

    Analog I/O added, not tested

--- pins.hpp
+++ pins.hpp
@@ -1,5 +1,12 @@
+#ifndef PINS_HPP
+#define PINS_HPP
+
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "hardware/adc.h"
+#include "hardware/pwm.h"
+
+#endif // PINS_HPP
 
 #define LED_PIN CYW43_WL_GPIO_LED_PIN
 
@@ -11,23 +18,23 @@ class Led{
             cyw43_arch_init();
         }
 
-        void high(){
-            cyw43_arch_gpio_put(LED_PIN, 1);
+        static void high(){
+            cyw43_arch_gpio_put(LED_PIN, true);
         }
 
-        void low(){
-            cyw43_arch_gpio_put(LED_PIN, 0);
+        static void low(){
+            cyw43_arch_gpio_put(LED_PIN, false);
         }
 
-        void set(bool value){
+        static void set(bool value){
             cyw43_arch_gpio_put(LED_PIN, value);
         }
 
-        void toggle(){
+        static void toggle(){
             cyw43_arch_gpio_put(LED_PIN, !cyw43_arch_gpio_get(LED_PIN));
         }
 
-        void panic(){
+        static void panic(){
             if(isPanic){
                 return;
             }
@@ -47,25 +54,25 @@ class PinOut{
     private:
         uint pin;
     public:
-        PinOut(uint pin){
+        explicit PinOut(uint pin){
             this->pin = pin;
             gpio_init(pin);
             gpio_set_dir(pin, GPIO_OUT);
         }
 
-        void high(){
-            gpio_put(pin, 1);
+        void high() const{
+            gpio_put(pin, true);
         }
 
-        void low(){
-            gpio_put(pin, 0);
+        void low() const{
+            gpio_put(pin, false);
         }
 
-        void set(bool value){
+        void set(bool value) const{
             gpio_put(pin, value);
         }
 
-        void toggle(){
+        void toggle() const{
             gpio_put(pin, !gpio_get(pin));
         }
 };
@@ -75,13 +82,62 @@ class PinIn{
     private:
         uint pin;
     public:
-        PinIn(uint pin){
+        explicit PinIn(uint pin){
             this->pin = pin;
             gpio_init(pin);
             gpio_set_dir(pin, GPIO_IN);
         }
 
-        bool get(){
+        [[nodiscard]] bool get() const{
             return gpio_get(pin);
+        }
+};
+
+class AnalogOut{
+    private:
+        uint pin;
+    public:
+        explicit AnalogOut(uint pin){
+            this->pin = pin;
+            adc_init();
+            adc_gpio_init(pin);
+        }
+
+        void write(uint value) const{
+                // Get a free PWM slice
+                uint slice_num = pwm_gpio_to_slice_num(this->pin);
+
+                // Set the frequency
+                pwm_set_clkdiv(slice_num, 1.f);
+
+                // Set the wrap value to 255 (like Arduino's analogWrite)
+                pwm_set_wrap(slice_num, 255);
+
+                // Set the PWM level
+                pwm_set_chan_level(slice_num, PWM_CHAN_A, value);
+
+                // Set the GPIO to be controlled by the PWM
+                gpio_set_function(pin, GPIO_FUNC_PWM);
+
+                // Enable the PWM
+                pwm_set_enabled(slice_num, true);
+        }
+};
+
+class AnalogIn{
+    private:
+        uint pin;
+        uint channel;
+    public:
+        AnalogIn(uint pin, uint channel){
+            this->pin = pin;
+            this->channel = channel;
+            adc_init();
+            adc_gpio_init(pin);
+        }
+
+        [[nodiscard]] uint read() const{
+            adc_select_input(this->channel);
+            return adc_read();
         }
 };
